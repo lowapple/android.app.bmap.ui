e4f2c4121610682bbdaccb1b1b5e507d4d0fe650
younjs <younjs@uniquegood.biz>
2022-08-23 21:49:48 +0900

    Initialize commit

---
+++ lib/main.dart
@@ -0,0 +1,205 @@
+import 'package:coordinator_layout/coordinator_layout.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:seoul_iot/pages/parking_lot_detail_page.dart';
+
+class AppSetting {
+  static const double defaultPadding = 24;
+}
+
+class AppPalette {
+  static const Color primary = Color(0xffBF4AD2);
+  static const Color primaryVariant = Color(0xFFF7BA34);
+  static const Color black = Color(0xFF000000);
+  static Color disable = Colors.black12;
+  static const Color background = Colors.white;
+
+  static Color enableOrDisableColor(bool value) {
+    return value ? primary : disable;
+  }
+}
+
+List<Future> systemChromeTask = [
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown])
+];
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Future.wait(systemChromeTask);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '서울시 IOT 해커톤',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: "/",
+      routes: {
+        "/": (context) => const MyHomePage(title: ""),
+        "/parking": (context) => const ParkingLotDetailPage()
+      },
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  bool _pinned = true;
+  bool _snap = false;
+  bool _floating = false;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            pinned: _pinned,
+            snap: _snap,
+            floating: _floating,
+            expandedHeight: 160.0,
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back_ios),
+              onPressed: () {},
+            ),
+            flexibleSpace: const FlexibleSpaceBar(
+              title: Text('SliverAppBar'),
+              centerTitle: true,
+              background: FlutterLogo(),
+            ),
+          ),
+          // const SliverToBoxAdapter(
+          //   child: SizedBox(
+          //     height: 20,
+          //     child: Center(
+          //       child: Text('Scroll to see the SliverAppBar in effect.'),
+          //     ),
+          //   ),
+          // ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Container(
+                  color: index.isOdd ? Colors.white : Colors.black12,
+                  height: 100.0,
+                  child: Center(
+                    child: Text('$index', textScaleFactor: 5),
+                  ),
+                );
+              },
+              childCount: 20,
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: OverflowBar(
+            overflowAlignment: OverflowBarAlignment.center,
+            children: <Widget>[
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  const Text('pinned'),
+                  Switch(
+                    onChanged: (bool val) {
+                      setState(() {
+                        _pinned = val;
+                      });
+                    },
+                    value: _pinned,
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  const Text('snap'),
+                  Switch(
+                    onChanged: (bool val) {
+                      setState(() {
+                        _snap = val;
+                        // Snapping only applies when the app bar is floating.
+                        _floating = _floating || _snap;
+                      });
+                    },
+                    value: _snap,
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  const Text('floating'),
+                  Switch(
+                    onChanged: (bool val) {
+                      setState(() {
+                        _floating = val;
+                        _snap = _snap && _floating;
+                      });
+                    },
+                    value: _floating,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Center buildBody(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            'You have pushed the button this many times:',
+          ),
+          Text(
+            '$_counter',
+            style: Theme.of(context).textTheme.headline4,
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/pages/parking_lot_detail_page.dart
@@ -0,0 +1,38 @@
+import 'package:coordinator_layout/coordinator_layout.dart';
+import 'package:flutter/material.dart';
+
+class ParkingLotDetailPage extends StatefulWidget {
+  const ParkingLotDetailPage({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _ParkingLotDetailPageState();
+}
+
+class _ParkingLotDetailPageState extends State<ParkingLotDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return CoordinatorLayout(
+      headerMaxHeight: 200,
+      headerMinHeight: kToolbarHeight + MediaQuery
+          .of(context)
+          .padding
+          .top,
+      headers: [
+        Builder(builder: (context) {
+          return SliverOverlapAbsorber(
+            handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
+            sliver: SliverSafeArea(
+              top: false,
+              sliver: SliverCollapsingHeader(
+                builder: (context, offset, diff) {
+                  return Text("ASDSASD");
+                },
+              ),
+            ),
+          );
+        }),
+      ],
+      body: Container(height: null, child: Text("ASD")),
+    );
+  }
+}
